53b8621cc6af7d89e74552ecfcce756150eb50df
joye <zhangzhizhuo@Hotmail.com>
2017-01-06 17:41:04 +0800

    add multi-Langage

---
+++ init.el
@@ -0,0 +1,17 @@
+(setq package-archives '(("gnu"   . "http://mirrors.tuna.tsinghua.edu.cn/elpa/gnu/")
+                         ("melpa" . "http://mirrors.tuna.tsinghua.edu.cn/elpa/melpa/")))
+(package-initialize) ;; You might already have this line
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (company-go company-php ## company-web company-lua lua-mode yasnippet whole-line-or-region toc-org tabbar switch-window smex pos-tip page-break-lines orgit org-fstree matlab-mode markdown-mode ido-ubiquitous hlinum hi2 hayoo ghci-completion fuzzy flycheck-hdevtools flycheck-haskell flex-isearch expand-region evil dired+ diminish company color-theme-sanityinc-tomorrow color-theme-sanityinc-solarized color-theme autopair auto-complete-clang auctex atom-dark-theme ac-math ac-haskell-process))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
