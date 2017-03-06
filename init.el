e399f3fce8f46d550f3b67a747e68797cf1e1f48
joye <zhangzhizhuo@Hotmail.com>
2017-03-06 17:35:36 +0800

    new commit

--- init.el
+++ init.el
@@ -1,17 +1,18 @@
-;;el-get安装文件
-(add-to-list 'load-path "~/.emacs.d/el-get/el-get")
-
-(unless (require 'el-get nil 'noerror)
-  (with-current-buffer
-      (url-retrieve-synchronously
-       "https://raw.githubusercontent.com/dimitri/el-get/master/el-get-install.el")
-    (goto-char (point-max))
-    (eval-print-last-sexp)))
-
-(add-to-list 'el-get-recipe-path "~/.emacs.d/el-get-user/recipes")
-(el-get 'sync)
 
 ;;显示行号
 (global-linum-mode t)
 ;;保存上次打开的场景
 (desktop-save-mode 1)
+
+
+;;go mode 插件
+(add-to-list 'load-path "~/.emacs.d/plugin/go-mode.el/")
+(require 'go-mode-autoloads)
+
+
+;;lua-mode 插件
+
+(add-to-list 'load-path "~/.emacs.d/plugin/lua-mode/")
+(autoload 'lua-mode "lua-mode" "Lua editing mode." t)
+(add-to-list 'auto-mode-alist '("\\.lua$" . lua-mode))
+(add-to-list 'interpreter-mode-alist '("lua" . lua-mode))
