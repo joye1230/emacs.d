1b1959e8b0ecd8f7ec1685a45b2cf6305cb8fe1e
joye <zhangzhizhuo@Hotmail.com>
2017-03-06 17:46:13 +0800

    增加language 文件夹

--- init.el
+++ init.el
@@ -6,13 +6,13 @@
 
 
 ;;go mode 插件
-(add-to-list 'load-path "~/.emacs.d/plugin/go-mode.el/")
+(add-to-list 'load-path "~/.emacs.d/plugin/language/")
 (require 'go-mode-autoloads)
 
 
 ;;lua-mode 插件
 
-(add-to-list 'load-path "~/.emacs.d/plugin/lua-mode/")
+(add-to-list 'load-path "~/.emacs.d/plugin/language/")
 (autoload 'lua-mode "lua-mode" "Lua editing mode." t)
 (add-to-list 'auto-mode-alist '("\\.lua$" . lua-mode))
 (add-to-list 'interpreter-mode-alist '("lua" . lua-mode))
