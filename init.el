cb121409f7ed7d34706521fd8dd0d371fc4b51bf
joye <zhangzhizhuo@Hotmail.com>
2017-03-06 15:10:10 +0800

    my config

--- init.el
+++ init.el
@@ -1,17 +1,17 @@
-(setq package-archives '(("gnu"   . "http://mirrors.tuna.tsinghua.edu.cn/elpa/gnu/")
-                         ("melpa" . "http://mirrors.tuna.tsinghua.edu.cn/elpa/melpa/")))
-(package-initialize) ;; You might already have this line
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   (quote
-    (company-go company-php ## company-web company-lua lua-mode yasnippet whole-line-or-region toc-org tabbar switch-window smex pos-tip page-break-lines orgit org-fstree matlab-mode markdown-mode ido-ubiquitous hlinum hi2 hayoo ghci-completion fuzzy flycheck-hdevtools flycheck-haskell flex-isearch expand-region evil dired+ diminish company color-theme-sanityinc-tomorrow color-theme-sanityinc-solarized color-theme autopair auto-complete-clang auctex atom-dark-theme ac-math ac-haskell-process))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+;;el-get安装文件
+(add-to-list 'load-path "~/.emacs.d/el-get/el-get")
+
+(unless (require 'el-get nil 'noerror)
+  (with-current-buffer
+      (url-retrieve-synchronously
+       "https://raw.githubusercontent.com/dimitri/el-get/master/el-get-install.el")
+    (goto-char (point-max))
+    (eval-print-last-sexp)))
+
+(add-to-list 'el-get-recipe-path "~/.emacs.d/el-get-user/recipes")
+(el-get 'sync)
+
+;;显示行号
+(global-linum-mode t)
+;;保存上次打开的场景
+(desktop-save-mode 1)
